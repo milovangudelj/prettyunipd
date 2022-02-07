55fdfe6727627cca3c1845762e617e2e8cc88e3e
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-02-07 16:10:41 +0100

    Removed layout from all pages except home and mods

--- website/components/Layout.tsx
+++ website/components/Layout.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useTheme } from "../lib/theme";
 import Divide from "./Divide";
@@ -5,13 +6,18 @@ import Footer from "./Footer";
 
 const Layout = (props) => {
 	const { dark } = useTheme();
+	const { pathname } = useRouter();
 
 	return (
 		<div className={dark ? "dark" : undefined}>
 			<div className="bg-grey-50 dark:bg-grey-900 min-h-screen">
 				{props.children}
-				<Divide />
-				<Footer text={props.footerText} />
+				{(pathname === "/" || pathname === "/mods") && (
+					<>
+						<Divide />
+						<Footer text={props.footerText} />
+					</>
+				)}
 			</div>
 		</div>
 	);
